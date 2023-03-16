baccab5e06373bec637bddcd05a5bf2d92226dac
joshgermon <joshgermon@gmail.com>
2023-03-16 23:11:21 +1100

    Initial Commit

---
+++ SwiftUI Sidebar App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI Sidebar App
+//
+//  Created by Joshua Germon on 16/3/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Sidebar App/SwiftUI_Sidebar_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Sidebar_AppApp.swift
+//  SwiftUI Sidebar App
+//
+//  Created by Joshua Germon on 16/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Sidebar_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
